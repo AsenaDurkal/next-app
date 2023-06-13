3e9eeb7afa81fdd4184a9406367ee886d61da212
Asena <asena.durkal@gmail.com>
2023-06-13 23:47:04 +0200

    feat:initial commit:tada

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+export default function About(){
+
+    return(
+        <main>
+            <h1 className="text-2xl font-semibold">
+                <div>
+                    <p className="mt-5"></p>
+                </div>
+            </h1>
+
+        </main>
+    )
+
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,16 @@
+function Contact(){
+    return(
+        <main>
+            <h1 className="text-2xl font-extrabold">
+            <div>
+                <p>
+                    To contact, please send an email.
+                </p>
+            </div>
+            </h1>
+           
+        </main>
+    );
+}
+
+export default Contact;
