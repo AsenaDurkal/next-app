cc9f12b0f8de5450f5e879eff3a16396a31ea9a3
Asena <asena.durkal@gmail.com>
2023-06-22 01:12:03 +0200

    feat:component with useState

--- app/dishes/page.tsx
+++ app/dishes/page.tsx
@@ -1,26 +1,34 @@
 import { Header } from "@/components/Header";
+import { useState } from "react";
 
+const mockdishes = [{
+
+    id:1,
+    Name: 'name 1',
+    categories: 'category 1'
+},
+
+{
+    id:2,
+    Name: 'name 2',
+    categories: 'category 2'
+}
+];
+interface Dish{
+    id:string;
+    Name:string;
+    categories:string
+} 
 function Dishes(){
 
-    const dishes = [{
+    const [dishes, setDishes] = useState<Dish[] | null>(null);
 
-        id:1,
-        Name: 'name 1',
-        categories: 'category 1'
-    },
-
-    {
-        id:2,
-        Name: 'name 2',
-        categories: 'category 2'
-    }
-];
     return(
         <main className="mt-6">
             
                 <Header>Dishes</Header>
                 <p  className="mt-5"> List of recipes</p>   
-                <div>{dishes.map((elem)=>{
+                <div>{dishes && dishes.map((elem)=>{
                     return(
                         <div key={elem.id}>{elem.Name},{elem.categories}</div>
                     )
